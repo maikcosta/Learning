4b8d005b3d682333727866203d995fdd439c7963
Maik Costa <maikcosta@gmail.com>
2020-02-27 13:24:45 -0300

    Insert JavaDocs

--- Java/exercicios/src/fundamentos/PrimeiroPrograma.java
+++ Java/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,17 @@
 package fundamentos;
 
+/**
+ * 
+ * @author Maik Costa <maikcosta@gmail.com>
+ * @since	2020
+ * @see <https://github.com/maikcosta/Learning>
+ */
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		System.out.println("Primeiro Programa Parte #01!!!");
 		System.out.println("Primeiro Programa Parte #02!!!");
+		System.out.println("Fim");
 	}
 }
