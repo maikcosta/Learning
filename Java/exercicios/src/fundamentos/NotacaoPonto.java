f82a9aa16125712e0263721c01808997769f5e17
Maik Costa <maikcosta@gmail.com>
2020-02-27 15:19:53 -0300

    Notacao ponto

---
+++ Java/exercicios/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+public class NotacaoPonto {
+	public static void main(String[] args) {
+		
+		String s = "Bom dia X";
+		s = s.toUpperCase();
+		System.out.println(s);
+		
+		s = s.replace("X", "Usuário");
+		System.out.println(s);
+		
+		
+		s = s.concat("!!!");
+		System.out.println(s);
+		
+		
+		String y = "Bom dia X".replace("X", "Gui");
+		System.out.println(y);
+		
+	}
+}
---
+++ Java/exercicios/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,35 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+		// Informações de funcionário
+		
+		
+		// Tipos numericos inteiros
+		byte anosDeEmpresa	=	23;
+		short numeroDeVoos	=	542;
+		int	id	=	56789;
+		long pontosAcumulados = 1_234_845_223L;
+		
+		// Tipos numeros reais
+		float salario	= 11_445.44F;
+		double vendasAcumuladas	=	2_991_797_103.01;
+		
+		//Tipos booleano
+		boolean estaDeFerias	=	false;
+		
+		//Tipo Caracter
+		char status	= 'A';
+		//Dias de empresa
+		System.out.println(anosDeEmpresa * 365);
+		//Número de viagens
+		System.out.println(numeroDeVoos / 2);
+		//Pontos por real
+		System.out.println(pontosAcumulados / vendasAcumuladas);
+		System.out.println(id + "ganha -> " + salario);
+		System.out.println("Férias " + estaDeFerias);
+		System.out.println("Status ->" + status);
+		
+	}
+
+}
