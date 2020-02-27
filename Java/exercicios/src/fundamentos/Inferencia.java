fd074c1166fd286b9b413a745ca120c44d5444cc
Maik Costa <maikcosta@gmail.com>
2020-02-27 14:21:09 -0300

    Learning Inferencia

---
+++ Java/exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		double a = 4.5;
+		System.out.println(a);
+		
+		var b = 4.5;
+		System.out.println(b);
+		
+		var c = "Texto";
+		System.out.println(c);
+		
+		double d ; // Variavel foi declarada
+		d= 123.65; // Variavel foi iniciada
+		System.out.println(d);
+		
+		var e = 123.65; 
+		System.out.println(e);
+	}
+}
