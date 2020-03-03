ba47577c3e01437a054bca34243484ca7ad0fc41
Maik Costa <maikcosta@gmail.com>
2020-03-03 17:04:04 -0300

    Final do dia 03/03/20

---
+++ Java/exercicios/src/controle/SwitchSemBreak.java
@@ -0,0 +1,22 @@
+package controle;
+
+public class SwitchSemBreak {
+	public static void main(String[] args) {
+		
+		// if (boolean)...
+		// while (boolean)...
+		// for (;boolean;)...
+		// switch ()
+		
+		String faixa = "preta";
+		switch(faixa.toLowerCase()) {
+		case "preta":
+			System.out.println("Sei o Bassai-Dai...");
+		case "marron":
+			System.out.println("Sei o Bassai-Dai...");			
+		}
+		
+		
+	}
+
+}
