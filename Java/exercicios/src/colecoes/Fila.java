32c59a95bb02fbcb38b25e393a46977cedec613c
Maik Costa <maikcosta@gmail.com>
2020-03-19 07:39:39 -0300

    Fila

---
+++ Java/exercicios/src/colecoes/Fila.java
@@ -0,0 +1,36 @@
+package colecoes;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Fila {
+	public static void main(String[] args) {
+		Queue<String> fila = new LinkedList<>();
+		
+		
+		// Offer e Add -> Adciona elementos na fila
+		//Diferença é o comportamento quando a fila está cheia!
+		fila.add("Ana");
+		fila.offer("Bia");
+		fila.add("Carlos");
+		fila.offer("Daniel");
+		fila.add("Rafaela");
+		fila.offer("Gui");
+		
+		System.out.println(fila.peek());
+		System.out.println(fila.peek());
+		System.out.println(fila.element());
+		System.out.println(fila.element());
+		
+		//fila.size();
+		//fila.clear();
+		//fila.isEmpty();
+		
+		System.out.println(fila.poll());
+		System.out.println(fila.remove());
+		System.out.println(fila.poll());
+		System.out.println(fila.poll());
+		System.out.println(fila.remove());
+	}
+
+}
