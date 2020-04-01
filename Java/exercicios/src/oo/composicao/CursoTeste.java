92f3938143658d960eae81321ffff3701cd4d731
Maik Costa <maikcosta@gmail.com>
2020-04-01 10:08:37 -0300

    oo n para n

---
+++ Java/exercicios/src/oo/composicao/CursoTeste.java
@@ -0,0 +1,31 @@
+package oo.composicao;
+
+public class CursoTeste {
+	public static void main(String[] args) {
+		Aluno aluno1 = new Aluno("João");
+		Aluno aluno2 = new Aluno("Maria");
+		Aluno aluno3 = new Aluno("Pedro");
+		
+		Curso curso1 = new Curso("Java Completo");
+		Curso curso2 = new Curso("Web 2023");
+		Curso curso3 = new Curso("React Native");
+		
+		
+		curso1.adicionarAluno(aluno1);
+		curso1.adicionarAluno(aluno2);
+		
+		
+		curso2.adicionarAluno(aluno1);
+		curso2.adicionarAluno(aluno2);
+		
+		aluno1.adicionarCursos(curso3);
+		aluno2.adicionarCursos(curso3);
+		aluno3.adicionarCursos(curso3);
+		
+		for (Aluno aluno: curso3.alunos) {
+			System.out.println("Estou matriculado no curso" + curso3.nome + "...");
+			System.out.println("... e meu nome é " + aluno.nome );
+		}
+		
+	}
+}
