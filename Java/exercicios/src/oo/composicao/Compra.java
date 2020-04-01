f72b6125b74e36e3c2777b3b1d957491d033ffce
Maik Costa <maikcosta@gmail.com>
2020-04-01 09:47:02 -0300

    oo Relação Bidirecional

--- Java/exercicios/src/oo/composicao/Compra.java
+++ Java/exercicios/src/oo/composicao/Compra.java
@@ -7,6 +7,11 @@ public class Compra {
 	String cliente;
 	ArrayList<Item> itens = new ArrayList<Item>();
 	
+	void adicionarItem(String nome, int quantidade, double preco) {
+		this.adicionarItem(new Item(nome,quantidade,preco));
+		
+	}
+	
 	void adicionarItem(Item item) {
 		itens.add(item);
 		item.compra = this;
--- Java/exercicios/src/oo/composicao/CompraTeste.java
+++ Java/exercicios/src/oo/composicao/CompraTeste.java
@@ -6,13 +6,15 @@ public class CompraTeste {
 		
 		Compra compra1 = new Compra();
 		compra1.cliente = "João";
-		compra1.adicionarItem(new Item("Caneta", 20, 7.45));
-		compra1.adicionarItem(new Item("Borracha", 12, 3.89));
-		compra1.adicionarItem(new Item("Caderno", 3, 18.79));
+		compra1.adicionarItem("Caneta", 20, 7.45);
+		compra1.adicionarItem("Borracha", 12, 3.89);
+		compra1.adicionarItem("Caderno", 3, 18.79);
 		
 		System.out.println(compra1.itens.size());
-		
 		System.out.println(compra1.obterValorTotal());
+		
+		double total = compra1.itens.get(0).compra.itens.get(1).compra.obterValorTotal();
+		System.out.println("O Total é R$" + total);
 
 	}
 
