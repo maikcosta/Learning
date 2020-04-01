f275f597ea1557f73154f993185e490640086649
Maik Costa <maikcosta@gmail.com>
2020-04-01 19:15:29 -0300

    Desafio

--- Java/exercicios/src/oo/composicao/desafio/Cliente.java
+++ Java/exercicios/src/oo/composicao/desafio/Cliente.java
@@ -1,5 +1,27 @@
 package oo.composicao.desafio;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cliente {
+	final String nome;
+	final List<Compra> compras = new ArrayList<Compra>();
+	
+	
+	Cliente(String nome){
+		this.nome = nome;
+		
+	}
+	
+	
+	double obrterValorTotal() {
+		double total = 0 ;
+		
+		for (Compra compra: compras) {
+			total += compra.obterValorTotal();
+		}
+		
+		return total;
+	}
+	
 }
