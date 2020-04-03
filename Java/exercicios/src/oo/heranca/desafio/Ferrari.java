4cd25674fbc02e28f938d76c21c9643d02bcd510
Maik Costa <maikcosta@gmail.com>
2020-04-03 14:02:26 -0300

    oo Heranã de construtor padrão

--- Java/exercicios/src/oo/heranca/desafio/Ferrari.java
+++ Java/exercicios/src/oo/heranca/desafio/Ferrari.java
@@ -1,8 +1,19 @@
 package oo.heranca.desafio;
 
-public class Ferrari extends Carro{
-	
-	void acelerar() {
-		velocidadeAtual += 15;
+public class Ferrari extends Carro {
+
+	Ferrari() {
+		super(315);
 	}
+
+	Ferrari(int velocidadeMaxima) {
+		super(velocidadeMaxima);
+		delta = 350;
+	}
+
+	/*
+	 * void acelerar() { velocidadeAtual += 15;
+	 * 
+	 * }
+	 */
 }
--- Java/exercicios/src/oo/heranca/desafio/Teste.java
+++ Java/exercicios/src/oo/heranca/desafio/Teste.java
@@ -3,7 +3,7 @@ package oo.heranca.desafio;
 public class Teste {
 	public static void main(String[] args) {
 		
-		Ferrari ferrari = new Ferrari();
+		Ferrari ferrari = new Ferrari(400);
 		ferrari.velocidadeAtual = 0;
 		
 		
