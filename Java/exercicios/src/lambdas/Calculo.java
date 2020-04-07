cedc631c824104b5b75fe2e5e01d88820fe088c4
Maik Costa <maikcosta@gmail.com>
2020-04-07 09:55:50 -0300

    Functions Lambdas

---
+++ Java/exercicios/src/lambdas/Calculo.java
@@ -0,0 +1,17 @@
+package lambdas;
+
+//@FunctionalInterface Força um único metodo
+@FunctionalInterface
+public interface Calculo {
+	
+	double executar (double a, double b);
+	
+	default String legal() {
+		return "Legal";
+	}
+	
+	static String muitoLegal() {
+		return "muito legal";
+	}
+
+}
