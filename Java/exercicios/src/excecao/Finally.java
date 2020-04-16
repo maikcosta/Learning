b1c30bf67ba2978b2dc8a9373e7968bb69a68e16
Maik Costa <maikcosta@gmail.com>
2020-04-16 12:49:24 -0300

    Finally

---
+++ Java/exercicios/src/excecao/Finally.java
@@ -0,0 +1,24 @@
+package excecao;
+
+import java.util.Scanner;
+
+public class Finally {
+	public static void main(String[] args) {
+		
+		System.out.print("Digite um numero: ");
+		Scanner entrada = new Scanner(System.in);
+		try {
+			System.out.println(7/entrada.nextInt());
+			
+			entrada.close();
+			
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			System.out.println("Finalmente...");
+			entrada.close();
+		}
+		System.out.println("Fim!");
+	}
+
+}
