76b7de6a084f8b25254e7d1c162c3a50daed08ae
Maik Costa <maikcosta@gmail.com>
2020-04-16 11:24:41 -0300

    Tratamento de erros

---
+++ Java/exercicios/src/excecao/personalizadaA/TesteValidacoes.java
@@ -0,0 +1,17 @@
+package excecao.personalizadaA;
+
+import streams.Aluno;
+
+public class TesteValidacoes {
+	public static void main(String[] args) {
+		try {
+			Aluno aluno = new Aluno("", -7);
+			Validar.aluno(aluno);
+		} catch (StringVaziaExpection e) {
+			System.out.println(e.get);
+		}
+		
+		System.out.println("Fim ;)");
+	}
+
+}
