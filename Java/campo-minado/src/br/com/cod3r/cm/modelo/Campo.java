62b1dd4995c6fe2367d17eefaa89fc3d29f91433
Maik Costa <maikcosta@gmail.com>
2020-04-27 11:11:36 -0300

    campo

--- Java/campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
+++ Java/campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -53,7 +53,16 @@ public class Campo {
 			if(minado) {
 				throw new ExplosaoException();
 			}
+			if (vizinhancaSegura()) {
+				vizinhos.forEach(v -> v.abrir());
+			}
+			return true;
+		} else {
+			return false;			
 		}
-		return false;
+
+	}
+	boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
 }
