09e693746f033b457b2b65f7281c9ea6b45eac77
Maik Costa <maikcosta@gmail.com>
2020-05-07 11:43:37 -0300

    Observer

---
+++ Java/exercicios/src/padroes/observer/AniversarioSupresa.java
@@ -0,0 +1,15 @@
+package padroes.observer;
+
+public class AniversarioSupresa {
+	public static void main(String[] args) {
+		Namorada namorada = new Namorada();
+		Porteiro porteiro = new Porteiro();
+		
+		porteiro.registrarObservador(namorada);
+		porteiro.registrarObservador(e -> {
+			System.out.println("Surpresa via lambda!");
+			System.out.println("Ocorreu em: " + e.getMomento());
+		});
+		porteiro.monitorar();
+	}
+}
