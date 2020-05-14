8f2c271b4d402af271199e6f3eb1be53681f64ee
Maik Costa <maikcosta@gmail.com>
2020-05-14 09:15:28 -0300

    Display

---
+++ Java/calculadora/src/br/com/cod3r/calc/visao/Botao.java
@@ -0,0 +1,9 @@
+package br.com.cod3r.calc.visao;
+
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class Botao extends JButton{
+	
+
+}
--- Java/calculadora/src/br/com/cod3r/calc/visao/Calculadora.java
+++ Java/calculadora/src/br/com/cod3r/calc/visao/Calculadora.java
@@ -1,14 +1,36 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class Calculadora extends JFrame{
 	public Calculadora() {
-		setVisible(true);
+		organizarLayout();
+		
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+		setSize(232,322);
+		setLocationRelativeTo(null);
+	}
+	
+	
+	
+	private void organizarLayout() {
+		setLayout(new BorderLayout());
+		
+		Display display = new Display();
+		display.setPreferredSize(new Dimension(233,60));
+		add(display, BorderLayout.NORTH);
+		Teclado teclado = new Teclado();
+		add(teclado,BorderLayout.CENTER);
 		
 	}
+
+
+
 	public static void main(String[] args) {
 		new Calculadora();
 	}
