de6c152b40907d789979d77d3c2fece80b892d2a
Maik Costa <maikcosta@gmail.com>
2020-05-14 13:00:11 -0300

    Calculator

--- Java/calculadora/src/br/com/cod3r/calc/visao/Display.java
+++ Java/calculadora/src/br/com/cod3r/calc/visao/Display.java
@@ -7,20 +7,30 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import br.com.cod3r.calc.modelo.Memoria;
+import br.com.cod3r.calc.modelo.MemoriaObservador;
+
 @SuppressWarnings("serial")
-public class Display extends JPanel {
+public class Display extends JPanel implements MemoriaObservador {
 	
 	private JLabel label;
 	
 	
 	public Display() {
+		Memoria.getInstancia().adicionarObservador(this);
+		
 		setBackground(new Color(46,49,50));
-		label = new JLabel("1234,56");
+		label = new JLabel(Memoria.getInstancia().getTextoAtual());
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("courier",Font.PLAIN, 30));
 		setLayout(new FlowLayout(FlowLayout.RIGHT, 10,25));
 		
 		add(label);
 	}
-
+	@Override
+	public void valorAlterado(String novoValor) {
+		label.setText(novoValor);
+		
+	}
+	
 }
