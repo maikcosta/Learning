3072b09960c6de43212f614025b014d44e4da9b7
Maik Costa <maikcosta@gmail.com>
2020-08-06 08:59:28 -0300

    DAO

---
+++ Java/exercicios/src/jdbc/DAOTeste.java
@@ -0,0 +1,13 @@
+package jdbc;
+
+public class DAOTeste {
+	public static void main(String[] args) {
+		DAO dao = new DAO();
+		
+		String sql = "INSERT INTO pessoas (nome) VALUES (?)";
+		
+		System.out.println(dao.incluir(sql,"Igor Costa"));
+		
+		dao.close();
+	}
+}
