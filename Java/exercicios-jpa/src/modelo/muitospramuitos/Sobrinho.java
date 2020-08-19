22ffc07d62ec1736000b1ac1a5b4218bf7ca6d56
Maik Costa <maikcosta@gmail.com>
2020-08-19 16:22:44 -0300

    Muitos para muitos

---
+++ Java/exercicios-jpa/src/modelo/muitospramuitos/Sobrinho.java
@@ -0,0 +1,21 @@
+package modelo.muitospramuitos;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Sobrinho {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	@ManyToMany
+	private List<Tio> tios;
+
+}
