0d4facb2fcc69ce301cacd6592428b0fd11ebf66
Maik Costa <maikcosta@gmail.com>
2020-09-02 15:39:59 -0300

    StackPane

--- Java/exercicios-javafx/src/layout/AppLayout.java
+++ Java/exercicios-javafx/src/layout/AppLayout.java
@@ -10,10 +10,12 @@ public class AppLayout extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Parent raiz = null;
-		//raiz = new TesteAnchorPane();
-		//raiz = new TesteBorderPane();
-		//raiz = new TesteFlowPane();
+		raiz = new TesteAnchorPane();
+		raiz = new TesteBorderPane();
+		raiz = new TesteFlowPane();
 		raiz = new TesteGridPane();
+		raiz = new TesteStackPane();
+		
 		
 		Scene principal = new Scene(raiz,800,600);
 		
