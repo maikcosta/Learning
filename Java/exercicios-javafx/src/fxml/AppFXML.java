2efd68fc74438732b18b5505ab59a129da73c864
Maik Costa <maikcosta@gmail.com>
2020-09-03 13:23:17 -0300

    AppLogin

--- Java/exercicios-javafx/src/fxml/AppFXML.java
+++ Java/exercicios-javafx/src/fxml/AppFXML.java
@@ -11,10 +11,12 @@ import javafx.stage.Stage;
 public class AppFXML extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		String arquivoCSS = getClass().getResource("/fxml/Login.css").toExternalForm();
 		URL arquivoJXML = getClass().getResource("/fxml/Login.fxml");
 		GridPane raiz = FXMLLoader.load(arquivoJXML);
 		
-		Scene cena = new Scene(raiz,350,400);
+		Scene cena = new Scene(raiz,350,350);
+		cena.getStylesheets().add(arquivoCSS);
 		
 		primaryStage.setResizable(false);
 		primaryStage.setTitle("Tela de Login");
---
+++ Java/exercicios-javafx/src/fxml/LoginControlador.java
@@ -0,0 +1,40 @@
+package fxml;
+
+import org.controlsfx.control.Notifications;
+
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginControlador {
+	@FXML
+	private TextField campoEmail;
+	@FXML
+	private PasswordField campoSenha;
+
+	public void entrar() {
+
+		boolean emailValido = campoEmail.getText().contentEquals("aluno@cod3r.com.br");
+		boolean senhaValida = campoSenha.getText().contentEquals("12345678");
+
+		if (emailValido && senhaValida) {
+			Notifications.create()
+			.position(Pos.CENTER)
+			.title("Login FXML")
+			.graphic(null)
+			.text("Login efetuado com Sucesso!")
+			.show();
+			
+		} else {
+			Notifications.create()
+			.position(Pos.CENTER)
+			.title("Login FXML")
+			.graphic(null)
+			.text("Usuário/Senha Inválidos")
+			.show();
+		}
+
+	}
+
+}
