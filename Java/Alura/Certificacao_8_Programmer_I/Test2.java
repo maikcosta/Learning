889e2a74bb0387ad1c22458bfe0a4288e486140e
Maik Costa <maikcosta@gmail.com>
2023-01-22 09:37:40 -0300

    TrCertificação 8 Programmer I

---
+++ Java/Alura/Certificacao_8_Programmer_I/Test2.java
@@ -0,0 +1,13 @@
+package Certificacao_8_Programmer_I;
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("a");
+        System.out.println("b");
+        System.out.printf("c");
+        System.out.print("d");
+        System.out.println("\n");
+        System.out.print("e");
+    }
+}
---
+++ Java/Alura/Certificacao_8_Programmer_I/Test4.java
@@ -0,0 +1,2 @@
+package Certificacao_8_Programmer_I;public class Test4 {
+}
